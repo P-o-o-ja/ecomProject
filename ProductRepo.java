ff4db3f66c9433f3de3f10f6d94262349378a95b
P-o-o-ja <115568722+P-o-o-ja@users.noreply.github.com>
2024-09-03 16:48:43 +0530

    Add files via upload

---
+++ ProductRepo.java
@@ -0,0 +1,15 @@
+package com.example.ecomProject.repository;
+
+import com.example.ecomProject.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Component
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+
+}
